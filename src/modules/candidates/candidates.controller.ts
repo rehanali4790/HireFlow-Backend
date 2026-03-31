e16ad655af61b41d7022fa70778e7032eec8c0ac
rehanali4790 <alirehan36@yahoo.com>
2026-03-31 18:24:13 +0500

    Remove NestJS files - using pure Node.js/Express backend

--- src/modules/candidates/candidates.controller.ts
+++
@@ -1,6 +0,0 @@
-import { Controller } from '@nestjs/common';
-
-@Controller('candidates')
-export class CandidatesController {
-  // TODO: Implement candidate endpoints
-}
--- src/modules/candidates/candidates.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@nestjs/common';
-import { DatabaseService } from '../../database/database.service';
-
-@Injectable()
-export class CandidatesService {
-  constructor(private db: DatabaseService) {}
-  // TODO: Implement candidate service methods
-}
